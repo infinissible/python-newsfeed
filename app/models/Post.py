6b9525440cfc259bd14ad14f7c9baf4b4f47dd82
infinissible <infinissible@gmail.com>
2022-08-12 17:56:11 -0700

    homepage views and filters

--- app/models/Post.py
+++ app/models/Post.py
@@ -10,8 +10,8 @@ class Post(Base):
     title = Column(String(100), nullable=False)
     post_url = Column(String(100), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
-    create_at = Column(DateTime, default=datetime.now)
-    update_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
