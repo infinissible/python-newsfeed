999709f1fc0166fc32e229edb60e0d3efe97a961
infinissible <infinissible@gmail.com>
2022-08-13 00:15:52 -0700

    set up api's for user and sessions

--- app/models/User.py
+++ app/models/User.py
@@ -6,23 +6,27 @@ import bcrypt
 
 salt = bcrypt.gensalt()
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
 
-    @validates('email')
+    @validates("email")
     def validate_email(self, key, email):
         # make sure email address contatins @ character
-        assert '@' in email
+        assert "@" in email
 
         return email
 
-    @validates('password')
+    @validates("password")
     def validate_password(self, key, password):
         assert len(password) > 4
 
         # encrypt password
-        return bcrypt.hashpw(password.encode('utf-8'), salt)
+        return bcrypt.hashpw(password.encode("utf-8"), salt)
+
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
